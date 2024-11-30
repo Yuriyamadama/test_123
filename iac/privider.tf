cdb6186706b431323c7156fe44c2a419537da9c0
Yuri Yamada <tanishizuigao@gmail.com>
2024-11-30 14:59:58 +0900

    test

--- iac/privider.tf
+++ iac/privider.tf
@@ -1,20 +1,16 @@
-# local 定義
-locals {
-    github_repository           = var.
-    project_id                  = var.project_id
-    region                      = var.region
-    terraform_service_account   = google_service_account.terraform.email
-    
-    # api 有効化用
-    services = toset([                         # Workload Identity 連携用
-        "iam.googleapis.com",                  # IAM
-        "cloudresourcemanager.googleapis.com", # Resource Manager
-        "iamcredentials.googleapis.com",       # Service Account Credentials
-        "sts.googleapis.com"                   # Security Token Service API
-    ])
-}
-  
+# https://blog.g-gen.co.jp/entry/using-terraform-via-github-actions
 
+# local 定義
+
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+} 
+
+#先にbucketを作成、名前の変更
+#ローカルでテスト
+#ファイルを移動
 
 # provider 設定
 #使用するprpviderやバックエンドの場所を設定
@@ -27,26 +23,14 @@ terraform {
     }
     required_version = ">= 1.3.0"
     backend "gcs" {
-        bucket = "myproject_terraform_tfstate"
+        bucket = "glassy-iridium-438509-u8-tfstate"
         prefix = "terraform/state"
     }
-}
-  
-## API の有効化(Workload Identity 用)
-## localで定義したservicesを指定
-## destroy時にサービスも無効化される
-resource "google_project_service" "enable_api" {
-  for_each                   = local.services
-  project                    = local.project_id
-  service                    = each.value
-  disable_dependent_services = true
-}
-    
+}    
 
-
-
-
+# https://cloud.google.com/blog/ja/products/identity-security/secure-your-use-of-third-party-tools-with-identity-federation?hl=ja
 # Workload Identity Pool 設定
+
 resource "google_iam_workload_identity_pool" "mypool" {
     provider                  = google-beta
     project                   = local.project_id
@@ -56,99 +40,43 @@ resource "google_iam_workload_identity_pool" "mypool" {
 }
   
 # Workload Identity Provider 設定
-resource "google_iam_workload_identity_pool_provider" "myprovider" {
-    provider                           = google-beta
-    project                            = local.project_id
-    workload_identity_pool_id          = google_iam_workload_identity_pool.mypool.workload_identity_pool_id
-    workload_identity_pool_provider_id = "myprovider"
-    display_name                       = "myprovider"
-    description                        = "GitHub Actions で使用"
+
+  
+resource "google_iam_workload_identity_pool_provider" "github_actions_oidc" {
+  project                            = local.project_id
+  workload_identity_pool_provider_id = "myprovider"
+  workload_identity_pool_id          = google_iam_workload_identity_pool.mypool.workload_identity_pool_id 
+  display_name              = "GitHub Actions OIDC Provider"
+  description                        = "GitHub Actions で使用"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com/"
+  }
+
+  attribute_mapping = {
+    "google.subject"          = "assertion.sub"
+    "attribute.repository"    = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+    "attribute.branch"        = "assertion.sub.extract('/heads/{branch}/')"
+  }
+
+  attribute_condition = "assertion.repository_owner=='sec-mik'"
+}
+
+# resource "google_iam_workload_identity_pool_provider" "myprovider" {
+#     provider                           = google-beta
+#     project                            = local.project_id
+#     workload_identity_pool_id          = google_iam_workload_identity_pool.mypool.workload_identity_pool_id
+#     workload_identity_pool_provider_id = "myprovider"
+#     display_name                       = "myprovider"
+#     description                        = "GitHub Actions で使用"
     
-    attribute_mapping = {
-        "google.subject"       = "assertion.sub"
-        "attribute.repository" = "assertion.repository"
-    }
+#     attribute_mapping = {
+#         "google.subject"       = "assertion.sub"
+#         "attribute.repository" = "assertion.repository"
+#     }
     
-    oidc {
-        issuer_uri = "https://token.actions.githubusercontent.com"
-    }
-}
-  
-# GitHub Actions が借用するサービスアカウント
-data "google_service_account" "terraform_sa" {
-    account_id = local.terraform_service_account
-}
-  
-# サービスアカウントの IAM Policy 設定と GitHub リポジトリの指定
-resource "google_service_account_iam_member" "terraform_sa" {
-    service_account_id = data.google_service_account.terraform_sa.id
-    role               = "roles/iam.workloadIdentityUser"
-    member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mypool.name}/attribute.repository/${local.github_repository}"
-}
-
-
-# locals {
-#     terraformadmin_project_id = "${var.project_id}"
-# }
-
-# # サービス アカウントのアクセス トークンを取得するために使用するプロバイダを「別で」作成します。
-# # プロバイダは「google」ですが、それに割り当てられている「impersonation」エイリアスにご注意ください。
-
-# # scopes
-# # 意味: このプロバイダーが GCP API にアクセスする際に必要な権限（スコープ）を指定しています。
-# # 用途: TerraformがGoogle Cloudリソースを操作するための権限を設定します。
-
-# provider "google" {
-#     alias = "impersonation"
-#     scopes = [
-#         "https://www.googleapis.com/auth/cloud-platform",
-#         "https://www.googleapis.com/auth/userinfo.email",
-#     ]
-# }
-
-# # サービス アカウントとしての認証で使用するアクセス トークンを取得するためのデータブロックを追加します。
-# # データブロックが先に指定した impersonation プロバイダとサービス アカウントを参照している点に注意してください。
-# data "google_service_account_access_token" "default" {
-#     provider               = google.impersonation
-#     target_service_account = google_service_account.terraform.email
-#     scopes                 = ["userinfo-email", "cloud-platform"]
-#     lifetime               = "1200s"
-# }
-
-# # サービス アカウントのアクセス トークンを使用するもう 1 つの「google」プロバイダを記述します。
-# # エイリアスがないため、これは Terraform コードの Google リソースに使用されるデフォルトのプロバイダになります。
-  
-# provider "google" {
-#     project         = local.terraformadmin_project_id
-#     region          = "asia-northeast1"
-#     access_token    = data.google_service_account_access_token.default.access_token
-#     request_timeout = "60s"
-# }
-
-# #サービス アカウントでリモート状態ファイルを更新する
-
-# terraform {
-#     required_providers {
-#         google  = {
-#             source  = "hashicorp/google"
-#             version = ">= 4.0.0"
-#         }
+#     oidc {
+#         issuer_uri = "https://token.actions.githubusercontent.com"
 #     }
-#     required_version = ">= 1.3.0"
-  
-#     backend "gcs" {
-#         bucket                      = google_storage_bucket.tfstate.name
-#         impersonate_service_account = google_service_account.terraform.email
-#     }
-# }
-
-# # デフォルト ネットワークの作成をスキップ
-# resource "google_org_policy_policy" "compute_skip_default_network_creation" {
-#   name = "organizations/${var.organization_id}/compute.skipDefaultNetworkCreation"
-#   parent     = "organizations/${var.organization_id}"
-#   spec {
-#     rules {
-#       enforce = "TRUE"
-#     }
-#   }
 # }
--- iac/service_account.tf
+++ iac/service_account.tf
@@ -1,37 +1,42 @@
 
-# Terraform 用サービスアカウントの作成
-resource "google_service_account" "terraform" {
-  account_id   = "terraform"
-  display_name = "terraform"
+# GitHub Actions が借用するサービスアカウント
+data "google_service_account" "terraform_sa" {
+    account_id = var.terraform_email
+}
+
+# サービスアカウントの IAM Policy 設定と GitHub リポジトリの指定、userとしてrepositoryを追加
+resource "google_service_account_iam_member" "terraform_sa" {
+    service_account_id = data.google_service_account.terraform_sa.id
+    role               = "roles/iam.workloadIdentityUser"
+    member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.mypool.name}/attribute.repository/${var.github_repository}"
 }
 
 # Terraform 用サービスアカウントに IAM ロールを付与
 # https://blog.g-gen.co.jp/entry/how-to-use-iam-resources-of-terraform
 # 基本的にiam_memberを使っておけばok
 
-resource "google_organization_iam_member" "storage_admin" {
-  org_id = var.organization_id
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.terraform.email}"
-}
+# resource "google_organization_iam_member" "storage_admin" {
+#   org_id = var.organization_id
+#   role   = "roles/storage.admin"
+#   member = "serviceAccount:${google_service_account.terraform.email}"
+# }
 
 # Within GCP, there is a hierarchy: Organization, Project, Resource
 # The IAM policies you mentioned behaves the same; however, works on different levels based on the hierarchy.
 # For example, the google_project_iam_member will update the IAM policy to grant a role to a new member on the project level.
 # The google_organization_iam_member will do the same thing, but on the Organization level (which is a level higher than the project.
 
-resource "google_project_iam_member" "org_policy_admin" {
-  project = var.project_id
-  role   = "roles/orgpolicy.policyAdmin"
-  member = "serviceAccount:${google_service_account.terraform.email}"
-}
+# resource "google_project_iam_member" "org_policy_admin" {
+#   project = var.project_id
+#   role   = "roles/orgpolicy.policyAdmin"
+#   member = "serviceAccount:${var.terraform_email}"
+# }
 
-resource "google_project_iam_member" "org_policy_admin" {
+resource "google_project_iam_member" "tokencreator" {
   project = var.project_id
   role   = "roles/iam.serviceAccountTokenCreator"
-  member = "serviceAccount:${google_service_account.terraform.email}"
+  member = "serviceAccount:${var.terraform_email}"
 }
-
 
 # Terraform コマンドを実行するユーザーアカウントに対し、「サービスアクセストークン作成者ロール」を付与します。
 
